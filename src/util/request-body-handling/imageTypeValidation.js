1abb2f72fd30377b1e79a1c5954cb3c0ff7bc1d9
diala <diala.alassaf@tatweer.sy>
2022-04-10 17:21:58 +0300

    fix

--- src/util/request-body-handling/imageTypeValidation.js
+++ src/util/request-body-handling/imageTypeValidation.js
@@ -1,11 +1,11 @@
-exports.imageTypeValidation = (value, {req, location, path}) => { 
-  if (Object.keys(req.files) == 0 || req.files[0] == undefined) {
-    return Promise.reject('Image should be with .jpg, .png, .jpeg mimeType.')
-  }
-  const mimeTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-  const typeStatus = mimeTypes.some(type => req.files.image[0].mimetype == type);
-  console.log(typeStatus);
-  if (!typeStatus) {
-    return Promise.reject('Image should be with .jpg, .png, .jpeg mimeType.')
-  }
+exports.imageTypeValidation = (value, {req, location, path}) => {
+  return Object.keys(req.files).length == 0 ? 
+    Promise.reject('Image should be with .jpg, .png, .jpeg mimeType.') : true;
+  
+  // const mimeTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+  // const typeStatus = mimeTypes.some(type => req.files.image[0].mimetype == type);
+  // console.log(typeStatus);
+  // if (!typeStatus) {
+  //   return Promise.reject('Image should be with .jpg, .png, .jpeg mimeType.')
+  // }
 }
